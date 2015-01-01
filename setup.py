2e7d475facddb7dbd446d493aa3205ad1c3ee99b
onpon4 <onpon4@riseup.net>
2014-12-31 20:38:33 -0500

    Release 1.5.4

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ executables = [
 ]
 
 setup(name = "Pacewar",
-      version = "1.5.4a0",
+      version = "1.5.4",
       description = "A fighting game with lots of ships",
       author = "Julian Marchant",
       author_email = "onpon4@riseup.net",
