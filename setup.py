21d5dfbab89928f4f336c9531f70f9e30df7a1a2
onpon4 <onpon4@riseup.net>
2015-12-30 21:24:27 -0500

    Added the readme for GNU/Linux distribution.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ executables = [
 ]
 
 setup(name = "Pacewar",
-      version = "1.6.3a0",
+      version = "1.6.3",
       description = "A fighting game with lots of ships",
       author = "Julian Marchant",
       author_email = "onpon4@riseup.net",
